f827dcd2737792cad05eaf26be0dcb71e29be8fb
grzegorzmroz <kapitan.prix@gmail.com>
2022-06-15 19:55:38 +0200

    Implemented tictactoe in windows forms

--- tictactoe v3/Form1.Designer.cs
+++ tictactoe v3/Form1.Designer.cs
@@ -30,26 +30,142 @@
         {
             this.components = new System.ComponentModel.Container();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudBoardSize = new System.Windows.Forms.NumericUpDown();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudPlayerCount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBoardSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPlayerCount)).BeginInit();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // gb1
+            // 
+            this.gb1.Location = new System.Drawing.Point(197, 12);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(400, 400);
+            this.gb1.TabIndex = 1;
+            this.gb1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(45, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 34);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Board size";
+            // 
+            // nudBoardSize
+            // 
+            this.nudBoardSize.Location = new System.Drawing.Point(45, 87);
+            this.nudBoardSize.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudBoardSize.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudBoardSize.Name = "nudBoardSize";
+            this.nudBoardSize.Size = new System.Drawing.Size(85, 20);
+            this.nudBoardSize.TabIndex = 5;
+            this.nudBoardSize.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Location = new System.Drawing.Point(42, 187);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(0, 13);
+            this.lblWin.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Number of Players";
+            // 
+            // nudPlayerCount
+            // 
+            this.nudPlayerCount.Location = new System.Drawing.Point(45, 154);
+            this.nudPlayerCount.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudPlayerCount.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudPlayerCount.Name = "nudPlayerCount";
+            this.nudPlayerCount.Size = new System.Drawing.Size(85, 20);
+            this.nudPlayerCount.TabIndex = 8;
+            this.nudPlayerCount.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(609, 450);
+            this.Controls.Add(this.nudPlayerCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.nudBoardSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.gb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudBoardSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPlayerCount)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudBoardSize;
+        private System.Windows.Forms.Label lblWin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudPlayerCount;
     }
 }
 
--- tictactoe v3/Form1.cs
+++ tictactoe v3/Form1.cs
@@ -12,9 +12,140 @@ namespace tictactoe_v3
 {
     public partial class Form1 : Form
     {
+        public int FreeSpace;
+        public int BoardSize;
+        public string[] Player = {"X","O","~","Z","@"};
+        public int PlayerCurrent;
+        public int PlayerCount;
+        public Button[][] buttons;
+        public bool Winner;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void CreateBoard(int size)
+        {
+            buttons = new Button[size][];
+            for (int i = 0; i < size; i++)
+            {
+                Button[] buttonscol = new Button[size];
+                for (int j = 0; j < size; j++)
+                {
+                    Button newbutton = new Button();
+                    newbutton.Location = new System.Drawing.Point(i*(400/size), j*(400/size));
+                    newbutton.Name = $"newbtn{i}{j}";
+                    newbutton.Size = new System.Drawing.Size((400/size),(400/size));
+                    newbutton.TabIndex = i+j;
+                    newbutton.Text = String.Empty;
+                    newbutton.UseVisualStyleBackColor = true;
+                    newbutton.Click += new EventHandler(BoardClick);
+                    buttonscol[j] = newbutton;
+                    gb1.Controls.Add(newbutton);
+                }
+                buttons[i] = buttonscol;
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            gb1.Controls.Clear();
+            BoardSize = (int)nudBoardSize.Value;
+            FreeSpace = BoardSize * BoardSize;
+            PlayerCurrent = 0;
+            Winner = false;
+            lblWin.Text = String.Empty;
+            PlayerCount = (int)nudPlayerCount.Value;
+            btnStart.Text = "Restart";
+            CreateBoard(BoardSize);
+        }
+        private void BoardClick(object sender,EventArgs e)
+        {
+            Button clicked = sender as Button;
+            if (clicked.Text == String.Empty && Winner==false)
+            {
+                clicked.Text = Player[PlayerCurrent];
+
+
+                FreeSpace--;
+                Winner = CheckWinner();
+                if (Winner)
+                {
+                    lblWin.Text = $"Player {Player[PlayerCurrent]} won";
+                }
+                else if (FreeSpace == 0)
+                {
+                    lblWin.Text = "Draw";
+                }
+                if (PlayerCurrent == PlayerCount-1)
+                {
+                    PlayerCurrent = 0;
+                }
+                else PlayerCurrent++;
+            }
+        }
+
+        public bool CheckWinner()
+        {
+            bool winner = false;
+            for (int i = 0; i < BoardSize; i++)
+            {
+                int similar = 0;
+                for (int j = 0; j < BoardSize-1; j++)
+                {
+                    if (buttons[i][j].Text == buttons[i][j + 1].Text && buttons[i][j].Text!=String.Empty) similar++;
+                }
+                if (similar == BoardSize - 1)
+                {
+                    winner = true;
+                    for (int j=0; j<BoardSize;j++)
+                    {
+                        buttons[i][j].BackColor = Color.Green;
+                    }
+                }
+            }
+            for (int i = 0; i < BoardSize; i++)
+            {
+                int similar = 0;
+                for (int j = 0; j < BoardSize - 1; j++)
+                {
+                    if (buttons[j][i].Text == buttons[j+1][i].Text && buttons[j][i].Text != String.Empty) similar++;
+                }
+                if (similar == BoardSize - 1)
+                {
+                    winner = true;
+                    for (int j = 0; j < BoardSize; j++)
+                    {
+                        buttons[j][i].BackColor = Color.Green;
+                    }
+                }
+            }
+            int similardiag = 0;
+            for (int i=0; i < BoardSize-1; i++)
+            {
+                if (buttons[i][i].Text == buttons[i + 1][i + 1].Text && buttons[i][i].Text != String.Empty) similardiag++;
+            }
+            if (similardiag == BoardSize - 1)
+            {
+                winner = true;
+                for (int i = 0; i < BoardSize - 1; i++)
+                {
+                    buttons[i][i].BackColor = Color.Green;
+                }
+            }
+            similardiag = 0;
+            for (int i = BoardSize -1; i > 0; i--)
+            {
+                if (buttons[i][BoardSize-i-1].Text == buttons[i -1][BoardSize-i].Text && buttons[i][BoardSize-i-1].Text != String.Empty) similardiag++;
+            }
+            if (similardiag == BoardSize - 1)
+            {
+                winner = true;
+                for (int i = BoardSize - 1; i >= 0; i--)
+                {
+                    buttons[i][BoardSize - i - 1].BackColor = Color.Green;
+                }
+                }
+            return winner;
         }
     }
 }
